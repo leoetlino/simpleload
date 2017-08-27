c06b45c5759c35c324b4db3d0c3d322f6aea12d1
Léo Lam <leo@innovatetechnologi.es>
2017-08-27 10:36:28 +0200

    Loader: Remove unneeded fail function

--- src/loader/Main.cpp
+++ src/loader/Main.cpp
@@ -5,36 +5,30 @@
 #include "loader/Boot.h"
 #include "loader/DI.h"
 
-static int Fail(const char* message = "failed")
-{
-  printf("%s\n", message);
-  return 1;
-}
-
 int main()
 {
   printf("Initialising DI, opening partition and reloading IOS...\n");
   if (!Boot::InitAndOpenPartition(true))
-    return Fail();
+    return 1;
 
   printf("Initialising DI and opening partition again...\n");
   std::optional<DI::Partition> game_partition = Boot::InitAndOpenPartition(false);
   if (!game_partition)
-    return Fail();
+    return 1;
 
   printf("Initialising apploader...\n");
   Boot::GameEntryFunction game_entry = Boot::InitApploader();
   if (!game_entry)
-    return Fail();
+    return 1;
 
   printf("Poking constants...\n");
   Boot::PokeConstants(*game_partition);
 
   printf("Cleaning up before launch...\n");
   if (!Boot::CleanupBeforeLaunch())
-    return Fail();
+    return 1;
 
   printf("Launching game (%p)\n", game_entry);
   game_entry();
-  return Fail();
+  return 1;
 }
